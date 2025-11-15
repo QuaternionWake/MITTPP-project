63428eab99f2ea85df627a0c791340302dee9abe
QuaternionWake <simep@proton.me>
2025-11-15 02:13:32 +0100

    Initial implementation of parseInt()

--- src/num-parse.zig
+++ src/num-parse.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const mem = std.mem;
+const ascii = std.ascii;
+const fmt = std.fmt;
+const math = std.math;
 
 const ParseError = error{
     InvalidCharacter,
@@ -9,9 +13,147 @@ const ParseError = error{
 };
 
 pub fn parseInt(T: type, string: []const u8) ParseError!T {
-    _ = string;
-    return error.Unimplemented;
+    const type_info = switch (@typeInfo(T)) {
+        .int => |info| info,
+        else => @compileError("found type " ++ @typeName(T) ++ ", parseInt only supports integers"),
+    };
+    const TempT = @Type(.{ .int = .{ .bits = @max(type_info.bits, 8), .signedness = .unsigned } });
+
+    // Parse the string into a more managable format
+    // Most of the more complex functionality is handled in this function
+    const chunks: NumberChunks = try .init(string);
+
+    // At least one of these two needs to exist
+    if (chunks.integer == null and chunks.decimal == null) return error.NoNumber;
+
+    const integer =
+        if (chunks.integer) |str| blk: {
+            const int = fmt.parseInt(TempT, str, 10) catch |err|
+                return convertError(err, chunks.sign);
+            if (int == 0) break :blk 0;
+            const pow_of_10 = try math.powi(TempT, 10, chunks.order_of_magnitude);
+            break :blk math.mul(TempT, int, pow_of_10) catch |err|
+                return convertError(err, chunks.sign);
+        } else 0;
+
+    const decimal =
+        if (chunks.decimal) |str| blk: {
+            const trailing_zeros_start = (mem.lastIndexOfAny(u8, str, digits[1..]) orelse break :blk 0) + 1;
+            const end = @min(trailing_zeros_start, chunks.order_of_magnitude);
+            // Can't overflow as end can't be greater than
+            // chunks.order_of_magnitude due to the line above
+            const oom = chunks.order_of_magnitude - end;
+
+            const dec = fmt.parseInt(TempT, str[0..end], 10) catch |err|
+                return convertError(err, chunks.sign);
+            const pow_of_10 = try math.powi(TempT, 10, oom);
+            break :blk math.mul(TempT, dec, pow_of_10) catch |err|
+                return convertError(err, chunks.sign);
+        } else 0;
+
+    const result = math.add(TempT, integer, decimal) catch |err|
+        return convertError(err, chunks.sign);
+
+    if (type_info.signedness == .unsigned) {
+        if (chunks.sign == .neg and result != 0) return error.Underflow;
+        return math.cast(T, result) orelse return error.Overflow;
+    }
+
+    // Handle case where we're parsing the smallest negative number for T,
+    // whose positive counterpart can't fit in T
+    if (result == @as(TempT, @intCast(math.maxInt(T))) + 1) return math.minInt(T);
+
+    // Make sure the result can fit into T
+    if (result > math.maxInt(T)) return convertError(error.Overflow, chunks.sign);
+
+    const signed_result: T = @intCast(result);
+    return if (chunks.sign == .pos)
+        signed_result
+    else
+        -signed_result;
 }
+
+fn convertError(err: fmt.ParseIntError, sign: NumberChunks.Sign) ParseError {
+    return switch (err) {
+        error.Overflow => switch (sign) {
+            .pos => error.Overflow,
+            .neg => error.Underflow,
+        },
+        error.InvalidCharacter => error.InvalidCharacter,
+    };
+}
+
+const NumberChunks = struct {
+    integer: ?[]const u8,
+    decimal: ?[]const u8,
+
+    sign: Sign,
+    order_of_magnitude: u8,
+
+    const Sign = enum { pos, neg };
+
+    pub fn init(string_: []const u8) ParseError!NumberChunks {
+        const string = mem.trim(u8, string_, &std.ascii.whitespace);
+
+        if (string.len == 0) return error.NoNumber;
+
+        const sign: Sign, const num_start: usize = switch (string[0]) {
+            '+' => .{ .pos, 1 },
+            '-' => .{ .neg, 1 },
+            else => .{ .pos, 0 },
+        };
+
+        const order_of_magnitude, const num_end = blk: {
+            const suffix_start = if (mem.lastIndexOfNone(u8, string, ascii.letters)) |idx|
+                idx + 1
+            else
+                return error.NoNumber;
+
+            // If there is no suffix
+            if (suffix_start == string.len) break :blk .{ 0, string.len };
+
+            const num_end = mem.indexOfAny(u8, string, allowed_whitespace) orelse suffix_start;
+            // Make sure there are no invalid characters between two separate chunks of whitespace
+            if (mem.indexOfNone(u8, string[num_end..suffix_start], allowed_whitespace) != null) return error.InvalidCharacter;
+            const order_of_magnitude = try parseSuffix(string[suffix_start..]);
+            break :blk .{ order_of_magnitude, num_end };
+        };
+
+        const decimal_point = mem.indexOfScalar(u8, string, '.');
+
+        const integer, const decimal = if (decimal_point) |dp| blk: {
+            const integer = if (num_start != dp) string[num_start..dp] else null;
+            const decimal = if (dp + 1 != num_end) string[dp + 1 .. num_end] else null;
+            break :blk .{ integer, decimal };
+        } else .{ string[num_start..num_end], null };
+
+        // Make sure there are no invalid characters in the digit parts
+        if (integer) |str| if (mem.indexOfNone(u8, str, digits) != null) return error.InvalidCharacter;
+        if (decimal) |str| if (mem.indexOfNone(u8, str, digits) != null) return error.InvalidCharacter;
+
+        return .{
+            .integer = integer,
+            .decimal = decimal,
+
+            .sign = sign,
+            .order_of_magnitude = order_of_magnitude,
+        };
+    }
+};
+
+fn parseSuffix(string: []const u8) !u8 {
+    for (suffixes, 1..) |suffix, i| {
+        if (ascii.eqlIgnoreCase(string, suffix)) {
+            return @intCast(i * 3);
+        }
+    }
+    return error.UnknownSuffix;
+}
+
+const suffixes: [11][]const u8 = .{ "K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No", "Dc" };
+
+const allowed_whitespace = " \t";
+const digits = "0123456789";
 
 const t = std.testing;
 
